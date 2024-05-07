9aefb54075ccf48f92bff8cf8cec0bb8f9a28e2c
asadbekdev07 <futureprotrader1007@gmail.com>
2024-05-07 11:13:02 +0500

    Update site-header responsive, fix issues, add button icon change logic

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ const elSiteHeader = document.querySelector('.site-header')
 
 elSitenavButton.addEventListener('click', function() {
   elSiteHeader.classList.toggle('site-header--open')
+  if (elSiteHeader.classList.contains('site-header--open')) {
+    elSitenavButton.innerHTML = `<img class="icon-burger-close" src="images/icon-close.svg" alt="icon close" width="18" height="18">`
+  } else {
+    elSitenavButton.innerHTML = `<img class="icon-burger-close" src="images/icon-hamburger.svg" alt="icon close" width="24" height="11">`
+  }
 })
